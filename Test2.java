4b61fd1197eddd4be9fded8828a4886aecf9ce24
rongsimin <747334721@qq.com>
2020-05-11 19:37:46 +0800

    git tracks changes

--- Test2.java
+++ Test2.java
@@ -4,5 +4,6 @@ public class Test2 {
 	public static void main(String[] args){
 		System.out.println("hello,world");
 		System.out.println("second update");
+		System.out.println("tracks changes");
 	}
 }
