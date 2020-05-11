acecb6bf3abe70d256290a921f3839b0dd4efcf6
rongsimin <747334721@qq.com>
2020-05-11 22:12:58 +0800

    modified tracks changes of files

--- Test2.java
+++ Test2.java
@@ -4,6 +4,6 @@ public class Test2 {
 	public static void main(String[] args){
 		System.out.println("hello,world");
 		System.out.println("second update");
-		System.out.println("tracks changes");
+		System.out.println("tracks changes of files");
 	}
 }
